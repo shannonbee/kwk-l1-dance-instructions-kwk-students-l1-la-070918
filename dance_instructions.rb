a0c54d85a3fd3b151ab78449829b7b5b880dafca
shannonbee <shannonbenyamin@gmail.com>
2018-07-09 23:21:16 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,5 +1,6 @@
-def GangnamStyle
+def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose" 
-  puts"Lift right foot"
+end
+  def skip_step
   puts""
 # Code your methods below
